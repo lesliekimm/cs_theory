519274822d63b9394f5e39afce8c772ace0c1567
leslie kimm <leslie.kimm@gmail.com>
2016-05-20 13:32:37 -0700

    interview prep questions

---
+++ technical_interview_questions.py
@@ -0,0 +1,13 @@
+# Question 1
+# Rotate a linked list to the right by k places. (The last k elements move to the front.)
+
+# Question 2
+# Reverse a binary search tree so its elements are arranged from largest to smallest.
+
+# Question 3
+# T1 and T2 are two large binary trees, with T1 bigger than T2. Create an algorithm to
+# determine if T2 is a subtree of T1.
+
+# Bonus problem I just invented: Question 4
+# Suppose you have implementation a ​*linked list*​ class. Can you use this structure to
+# construct a ​*binary search tree*​ without defining a new class?
