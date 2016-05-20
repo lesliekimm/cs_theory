eaf4dde6b6a153ba1fffdff598bbe59e347a0fd8
leslie kimm <leslie.kimm@gmail.com>
2016-05-20 13:32:21 -0700

    print matrix in spiral order added

--- spiral.py
+++ spiral.py
@@ -1,3 +1,67 @@
+mtx1 = [[1]]
+
+mtx2 = [[ 1,  2,  3,  4],
+        [ 5,  6,  7,  8],
+        [ 9, 10, 11, 12]]
+
+mtx3 = [[ 1,  2,  3,  4]]
+
+mtx4 = [[1],
+        [2],
+        [3],
+        [4]]
+
+mtx5 = [[ 1,  2,  3,  4,  5],
+        [ 6,  7,  8,  9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20]]
+
+def print_spiral(mtx):
+  count = 1
+  print_numbers(mtx, 0, len(mtx)-1, 0, len(mtx[0])-1, count)
+
+  return
+
+def print_numbers(mtx, beg_r, end_r, beg_c, end_c, count):
+  area = len(mtx) * len(mtx[0])
+
+  if count <= area:
+    for x in range(beg_c, end_c+1):
+      print mtx[beg_r][x]
+      count += 1
+
+  if count <= area:
+    for y in range(beg_r+1, end_r+1):
+      print mtx[y][end_c]
+      count += 1
+
+  if count <= area:
+    for x in range(end_c-1, beg_c-1, -1):
+      print mtx[end_r][x]
+      count += 1
+
+  if count <= area:
+    for y in range(end_r-1, beg_r, -1):
+      print mtx[y][beg_c]
+      count += 1
+
+  if count <= area:
+    print_numbers(mtx, beg_r+1, end_r-1, beg_c+1, end_c-1, count)
+
+  return
+
+print_spiral(mtx1)
+print "--------"
+print_spiral(mtx2)
+print "--------"
+print_spiral(mtx3)
+print "--------"
+print_spiral(mtx4)
+print "--------"
+print_spiral(mtx5)
+print "--------"
+
+
 def spiral(r, c):
   mtx = [[0 for x in range(c)] for y in range(r)]
   count = 1
